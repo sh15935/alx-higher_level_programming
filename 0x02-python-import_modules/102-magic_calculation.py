e7e8d56900da142f9e73d56f53c0d0516fcd77e2
sh15935 <safazsahih@gmail.com>
2023-08-13 18:29:01 -0700

    tasks

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-wIdef magic_calculation(a, b):
+def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -8,6 +8,5 @@ wIdef magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-
     else:
         return (sub(a, b))
