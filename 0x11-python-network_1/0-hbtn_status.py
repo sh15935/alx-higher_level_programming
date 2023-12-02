26ddfc26dadc9dfeaf8a59b7d2828dbcfcf1b24c
sh15935 <safazsahih@gmail.com>
2023-12-02 21:20:17 +0000

    tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status with urllib"""
+""" Fetches  https://intranet.hbtn.io/status with urllib"""
 from urllib import request
 
 if __name__ == '__main__':
-url = "https://intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
+    
+    # Add headers to the request
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    req = request.Request(url, headers=headers)
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-req = request.Request(url, headers=headers)
-
-try:
-with request.urlopen(req) as response:
-r = response.read()
-s = "Body response:\n\t- type: {}\n\t- content: {}"
-s += "\n\t- utf8 content: {}"
-print(s.format(type(r), r, r.decode('utf-8')))
-except Exception as e:
-print("An error occurred: {}".format(e))
+    try:
+        with request.urlopen(req) as response:
+            r = response.read()
+            s = "Body response:\n\t- type: {}\n\t- content: {}"
+            s += "\n\t- utf8 content: {}"
+            print(s.format(type(r), r, r.decode('utf-8')))
+    except Exception as e:
+        print("An error occurred: {}".format(e))
